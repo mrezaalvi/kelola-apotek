9820880b432c139961e01c08889ef2bc738bb892
Muhammad Reza Alvi <mrezaalvi@gmail.com>
2023-10-21 22:59:20 +0700

    Modifying logo view component to get logo image and app name from settings

--- resources/views/vendor/filament-panels/components/logo.blade.php
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -1,4 +1,12 @@
 <div class="flex items-center gap-4">
-    <img src="{{ asset('/images/default.png') }}" alt="Logo" class="h-10">
-    <span class="whitespace-nowrap text-lg text-primary-600 font-semibold">APOTEK AR-RAHMAH</span>
+    @if(apotekLogo() && Storage::disk('files-apotek')->exists(apotekLogo()))
+        <img src="{{ Storage::disk('files-apotek')->url(apotekLogo()) }}" alt="Logo" class="h-10">
+    @else
+        <img src="{{ asset('/images/default.png') }}" alt="Logo" class="h-10">
+    @endif
+    @if(trim(apotekName()))
+        <span class="whitespace-nowrap text-lg text-primary-600 font-semibold">{{apotekName()}}</span>
+    @else
+        <span class="whitespace-nowrap text-lg text-primary-600 font-semibold">Kelola Apotek</span>
+    @endif
 </div>
